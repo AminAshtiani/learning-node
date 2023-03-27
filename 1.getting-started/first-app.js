817317c2f969cc85cc10180892be6715eb09f3db
Amin <amin.ashtiani@digikala.com>
2023-03-27 22:54:47 +0330

    learning-node: getting started documentation

--- 1.getting-started/first-app.js
+++ 1.getting-started/first-app.js
@@ -1,3 +1,6 @@
+// fs is package to work with files in node.js
 const fs = require("fs");
 
+// create and open a file to write your content
+// first argument is path of file, second is the content
 fs.writeFileSync("./hello.txt", "Hello from Node.js");
